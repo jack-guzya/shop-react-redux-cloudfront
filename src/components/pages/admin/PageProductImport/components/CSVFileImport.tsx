eb0569e4a7876409e36c5ba440979da118dec531
Yauheni_Huziankou <Yauheni_Huziankou@epam.com>
2021-09-18 13:44:29 +0300

    Fix CSV file import

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, { useState, ChangeEvent } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
 import axios from 'axios';
 
@@ -9,6 +9,25 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+// Windows appears the own mime type to .csv file. 
+// Presigned URL contains text/csv mime restriction 
+function convertCsvMimeType(type: string): string {
+  const csvMimeTypes = [ 
+    'text/comma-separated-values', 
+    'text/csv', 
+    'application/csv', 
+    'application/excel', 
+    'application/vnd.ms-excel', 
+    'application/vnd.msexcel', 
+    'text/anytext', 
+    'text/plain'
+  ];
+
+  return csvMimeTypes.includes(type) ?
+    ( console.info(`Type converting: ${type} => text/csv`), "text/csv") :
+    type
+}
+
 type CSVFileImportProps = {
   url: string,
   title: string
@@ -16,21 +35,25 @@ type CSVFileImportProps = {
 
 export default function CSVFileImport({url, title}: CSVFileImportProps) {
   const classes = useStyles();
-  const [file, setFile] = useState<any>();
+  const [file, setFile] = useState<File | null>();
 
-  const onFileChange = (e: any) => {
+  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e);
-    let files = e.target.files || e.dataTransfer.files
-    if (!files.length) return
+    let files = e.target.files;
+    if (!files?.length) return
     setFile(files.item(0));
   };
 
   const removeFile = () => {
-    setFile('');
+    setFile(null);
   };
 
   const uploadFile = async (e: any) => {
       // Get the presigned URL
+      if(!file) {
+        return;
+      }
+
       const response = await axios({
         method: 'GET',
         url,
@@ -42,10 +65,13 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
       console.log('Uploading to: ', response.data)
       const result = await fetch(response.data, {
         method: 'PUT',
-        body: file
+        body: file,
+        headers: {
+          "Content-Type": convertCsvMimeType(file.type)
+        }
       })
       console.log('Result: ', result)
-      setFile('');
+      setFile(null);
     }
   ;
 
@@ -55,7 +81,7 @@ export default function CSVFileImport({url, title}: CSVFileImportProps) {
         {title}
       </Typography>
       {!file ? (
-          <input type="file" onChange={onFileChange}/>
+          <input type="file" onChange={onFileChange} accept=".csv"/>
       ) : (
         <div>
           <button onClick={removeFile}>Remove file</button>
