3ae2e5994b01a23f157dd362e244f75330d3b000
Yauheni_Huziankou <Yauheni_Huziankou@epam.com>
2021-10-22 21:31:49 +0300

    Add card-api endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,12 +1,12 @@
 const DEFAULT_API_PATH = "https://ja4ylhyni1.execute-api.eu-west-1.amazonaws.com/dev"
-const { REACT_APP_API_PATH } = process.env;
+const { REACT_APP_API_PATH, REACT_APP_CARD_API_PATH } = process.env;
 
 const API_PATHS = {
   products: `${REACT_APP_API_PATH || DEFAULT_API_PATH}`,
-  order: `${REACT_APP_API_PATH || DEFAULT_API_PATH}`,
+  order: `${REACT_APP_CARD_API_PATH || DEFAULT_API_PATH}`,
   import: `${REACT_APP_API_PATH || DEFAULT_API_PATH}`,
   bff: `${REACT_APP_API_PATH || DEFAULT_API_PATH}`,
-  cart: `${REACT_APP_API_PATH || DEFAULT_API_PATH}`,
+  cart: `${REACT_APP_CARD_API_PATH || DEFAULT_API_PATH}`,
 };
 
 export default API_PATHS;
